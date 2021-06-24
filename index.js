13c89e4d0571ed1421da0aa967d637784a0427a8
userpan8 <86093416+userpan8@users.noreply.github.com>
2021-06-24 19:58:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -140,6 +140,31 @@ function run(bduss) {
   })
 }
 
+function send(){
+var request = require('request');
+var url="http://baidu.com";
+var requestData="上送的数据";
+ 
+httprequest(url,requestData);
+ 
+function httprequest(url,data){
+    request({
+        url: url,
+        method: "POST",
+        json: true,
+        headers: {
+            "content-type": "application/json",
+        },
+        //body: requestData
+    }, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body) // 请求成功的处理逻辑
+        }
+    });
+};
+
+}
+send();
 var args = process.env.BDUSS
 var param = args.split('&&')
 console.log(param)
